0d704e23e55f4e3ce9072f8ac0534d7789fb3eaa
Gwannon <monclus.jorge@gmail.com>
2021-09-23 12:32:44 +0200

    Mejoras en la visibilizacion de las tablas

--- info-campanas/index.php
+++ info-campanas/index.php
@@ -3,22 +3,27 @@
   <title>INFO CAMPAÑAS</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script>
     var offset = 0;
     var limit = <?php echo AC_API_LIMIT; ?>;
   </script>
   <script src="./assets/js/app.js"></script>
+  <style>
+    body {
+    	font-family: 'Ubuntu', cursive;
+    }
+  </style>
 </head>
 <body>
   <section>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-12">
-          <table class="table">
-            <thead>
+        <div class="col-12 p-0">
+          <table class="table table-striped">
+            <thead class="table-dark">
               <tr>
                 <th scope="col">Nombre</th>
                 <th scope="col">Título</th>
@@ -31,10 +36,12 @@
               </tr>
             </thead>
             <tbody></tbody>
-	        </table>
+          </table>
+        </div>
+        <div class="col-12 p-3">
           <div id="loading" style="display: none;">Cargando ...</div>
           <a href="#" id="loadmore" class="btn btn-primary" style="display: none;">Cargar más</a>
-	      </div>      
+        </div>      
       </div>
     </div>
   </section>
--- info-campanas/lib/curl.php
+++ info-campanas/lib/curl.php
@@ -1,19 +1,19 @@
 <?php
 
 function getAllCampaigns($max_items, $offset = 0) {
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, AC_API_DOMAIN."/api/3/campaigns?orders[sdate]=DESC&offset=".$offset."&limit=".$max_items);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
-	$result = json_decode(curl_exec($curl));
-	return $result->campaigns;
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, AC_API_DOMAIN."/api/3/campaigns?orders[sdate]=DESC&offset=".$offset."&limit=".$max_items);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
+  $result = json_decode(curl_exec($curl));
+  return $result->campaigns;
 }
 
 function getMessage($link) {
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, $link);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
-	$result = json_decode(curl_exec($curl));
-	return $result->campaignMessage;
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $link);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
+  $result = json_decode(curl_exec($curl));
+  return $result->campaignMessage;
 }
