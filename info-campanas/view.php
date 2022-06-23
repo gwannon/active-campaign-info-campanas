721809f655155bec982698a248896954c7c7eed0
Gwannon <monclus.jorge@gmail.com>
2022-06-23 14:52:31 +0200

    Añadimos opacidad a las fotos cuando se activa el mapa de calor.

--- info-campanas/view.php
+++ info-campanas/view.php
@@ -27,7 +27,7 @@ $extra = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquer
       },
       type : 'GET',
       dataType : 'json',
-      beforeSend: function () { },
+      beforeSend: function () { jQuery('a img').css('opacity', '1'); },
       success : function(json) {
         var uniquelinkclicks = json.uniquelinkclicks;
         var linkclicks = json.linkclicks;
@@ -66,7 +66,7 @@ $extra = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquer
         }
       },
       error : function(xhr, status) { },
-      complete : function(xhr, status) { }
+      complete : function(xhr, status) { jQuery('a img').css('opacity', '0.45'); }
     });
   }
 
